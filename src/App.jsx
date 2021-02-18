f68809e6b35000613450fbc061404555f8efd09b
Antoine Daurat <antoinedaurat@gmail.com>
2021-02-18 11:40:12 +0100

    v2

--- src/App.jsx
+++ src/App.jsx
@@ -3,176 +3,159 @@ import {
     BrowserRouter as Router, Switch, Route, Link
 } from 'react-router-dom';
 import ReactPlayer from "react-player";
-import { IconContext } from 'react-icons';
-import { FaGithub, FaFacebook, FaSoundcloud, FaYoutube } from 'react-icons/fa';
+import {IconContext} from 'react-icons';
+import {FaGithub, FaFacebook, FaSoundcloud, FaYoutube, FaChevronLeft, FaChevronRight} from 'react-icons/fa';
 
 import './App.scss';
 import {links} from './music-links';
 
 
 function App() {
+    const [faqIndex, setFaqIndex] = React.useState(-1);
+    const [linkIdx, setLinkIdx] = React.useState(0);
     return (
-        <div className="App">
-            {/*<header className="header">*/}
-            {/*</header>*/}
-            {/*<div className="main-container">*/}
-            <img src={`http://localhost:3000/bgnd.png`}
-                 className={"background-beeth"}
-                 alt={""}
-            />
-            <Router>
-                <div className={"sidebar"}>
-                    <div className={"sidebar-content"}>
-                        <img src={window.location.origin + "/ktonal-logo.png"} className="App-logo" alt="logo"/>
-                        <Link to={"/what-the-k"} className={"link"}>What the k?</Link>
-                        <Link to={"/code"} className={"link"}>Code</Link>
-                        <Link to={"/music"} className={"link"}>Music</Link>
-                        {/*<Link to={"/writings"} className={"link"}>Writings</Link>*/}
-                        {/*<Link to={"/project-gallery"} className={"link"}>Projects Gallery</Link>*/}
-                        {/*<Link to={"/members"} className={"link"}>Members</Link>*/}
+        <div className="main">
+            <div className={"ktonal stripe"}>
+                {/*<div className={"top"}><h2 style={{margin: "0 auto", width: "max-content"}}>K T O N A L</h2></div>*/}
+                <div className={"header"}>
+                    <div className={"what-the-k"}>
+                        <h1>What the </h1>
+                        <img src={window.location.origin + "/k-circle-bw.svg"} className="k-logo"
+                         alt="logo"/>
+                        <h1> ? </h1>
+                    </div>
+                    <div className={"faq description"}>
+                        {/*<div className={"answer"}>*/}
+                        <p>ktonal is a group of composers dedicated to AI-powered sound generation.</p>
+                        <p>We conduct our own research, write code and compose music.</p>
+                        {/*</div>*/}
                     </div>
                 </div>
-                <Switch>
-                    <div className={"content-container"}>
-                        <Route path={"/what-the-k"}>
-                            <div>
+                {/*<div>*/}
+                {/*    {faqIndex === 0 &&*/}
+                {/*    <div className={"answer"}>*/}
+                {/*        <p>ktonal is a group of composers dedicated to AI-powered sound generation.</p>*/}
+                {/*        <p>We conduct our own research, write code and compose music.</p>*/}
+                {/*    </div>}*/}
+                {/*    {faqIndex === 1 &&*/}
+                {/*    <div className={"answer"}>*/}
+                {/*        <p>We combine a developers perspective with composers interests.</p>*/}
+                {/*        <p>Though AI opens a whole new universe of possibilities, few systems are taylored to*/}
+                {/*            experimental artistic usage.</p>*/}
+                {/*        <p>Current AI/Deep-Learning systems for sounds and music are huge, expensive and hard to*/}
+                {/*            personalize.</p>*/}
+                {/*        <p>That sucks...</p>*/}
+                {/*        <p>We develop accessible, open-source tools for our own work and anybody interested.</p>*/}
+                {/*    </div>}*/}
+                {/*    {faqIndex === 2 &&*/}
+                {/*    <div className={"answer"}>*/}
+                {/*        <p>Spell the "k", then say "tonal".</p>*/}
+                {/*        <p>The "k" stands for the german word for AI : "Künstliche Intelligenz".</p>*/}
+                {/*    </div>}*/}
+                {/*    {faqIndex === 3 &&*/}
+                {/*    <div className={"answer"}>*/}
+                {/*        <p>*/}
+                {/*            Antoine Daurat*/}
+                {/*        </p>*/}
+                {/*        <p>*/}
+                {/*            Björn Erlach*/}
+                {/*        </p>*/}
+                {/*        <p>*/}
+                {/*            Roberto Fausti*/}
+                {/*        </p>*/}
+                {/*        <p>*/}
+                {/*            Genoël von Lilienstern*/}
+                {/*        </p>*/}
+                {/*        <p>*/}
+                {/*            Dohi Moon*/}
+                {/*        </p>*/}
+                {/*    </div>}*/}
+                {/*</div>*/}
+            </div>
+            <div className={"music stripe"}>
+                <div className={"intro description"}>
+                    <p>Lorem Ipsum, music music.</p>
+                    <p>Follow us on {"  "}
+                        <a href="https://youtube.com/" target="_blank" rel="noreferrer">
+                            YouTube
+                        </a>{" "}and{" "}
+                        <a href="https://soundcloud.com/ktonal" target="_blank" rel="noreferrer">
+                            SoundCloud
+                        </a>
+                        {" "}to get the latest tunes.</p>
+                </div>
+                <div className={"player"}>
+                        <span className={"previous"}
+                              onClick={() => setLinkIdx(linkIdx > 0 ? (linkIdx - 1) % links.length : links.length - 1)}>
+                        <IconContext.Provider value={{size: 40, color: '#666666'}}>
+                            <FaChevronLeft/>
+                        </IconContext.Provider>
+                        </span>
+                    <ReactPlayer url={links[linkIdx].url}
+                                 width={"80%"}
+                                 height={"30vh"}
+                    />
+                    <span className={"next"} onClick={() => setLinkIdx((linkIdx + 1) % links.length)}>
+                        <IconContext.Provider value={{size: 40, color: '#666666'}}>
+                            <FaChevronRight/>
+                        </IconContext.Provider>
+                        </span>
+                </div>
+                <h1>Music</h1>
+            </div>
+            <div className={"code stripe"}>
+                <div className={"header"}>
+                    <h1>Code</h1>
+                    <div className={"description"}>
+                        <p>We are working on several open-source projects to facilitate and democratize audio generation
+                            with state-of-the-art algorithms.</p>
+                        <p>You'll find all of them on our {" "}
+                            <a href="https://github.com/ktonal" target="_blank"
+                               rel="noreferrer">
+                                GitHub
+                            </a>
+                            .
+                        </p>
 
-                                <h2>What's ktonal?</h2>
-                                <p>ktonal is a group of composers dedicated to AI-powered sound generation.</p>
-                                <p>We conduct our own research, write code and compose music.</p>
-
-                                <h2>What's our deal?</h2>
-                                <p>We combine a developers perspective with composers interests.</p>
-                                <p>Though AI opens a whole new universe of possibilities, few systems are taylored to experimental artistic usage.</p>
-                                <p>Current AI/Deep-Learning systems for sounds and music are huge, expensive and hard to personalize.</p>
-                                <p>That sucks...</p>
-                                <p>We develop accessible, open-source tools for our own work and anybody interested.</p>
-
-                                <h2>How do you pronounce "ktonal"?</h2>
-                                <p>Spell the "k", then say "tonal".</p>
-                                <p>The "k" stands for the german word for AI : "Künstliche Intelligenz".</p>
-
-                                <h2>Who is ktonal?</h2>
-                                <div className={"members-list"}>
-                                    <p>
-                                        Antoine Daurat
-                                    </p>
-                                    <p>
-                                        Björn Erlach
-                                    </p>
-                                    <p>
-                                        Roberto Fausti
-                                    </p>
-                                    <p>
-                                        Genoël von Lilienstern
-                                    </p>
-                                    <p>
-                                        Dohi Moon
-                                    </p>
-
-                                </div>
-                            </div>
-                        </Route>
-                        <Route path={"/code"}>
-                            <div>
-                                <h2>Code</h2>
-                                <p>We open-source all the code we develop.</p>
-                                <p>{"If you want to get a practical first impression, open up our "}
-                                    <a href={"https://colab.research.google.com/github/k-tonal/mimikit-notebooks/blob/main/Docs/Mimikit_Get_Started.ipynb"}>
-                                        Getting Started notebook in colab</a>.</p>
-
-                                <h2>Repositories</h2>
-                                <section>You'll find all repos on our <a href={"https://github.com/k-tonal"}>GitHub
-                                    page</a>.
-                                    <h3><a href={"https://github.com/k-tonal/mimikit"}><code>mimikit</code></a></h3>
-                                    <p>python package for making deep generative models with your own
-                                        audio files.</p>
-
-                                    <h3><a
-                                        href={"https://github.com/k-tonal/mimikit-notebooks"}>the <code>mimikit-notebooks</code></a>
-                                    </h3>
-                                    <p>set of plug & play notebooks to use mimikit in colab.</p>
-                                    <h3><a href={"https://github.com/k-tonal"}><code>axx</code></a></h3>
-                                    <p>webapp still in early development to visualize and listen to the
-                                        models made with <code>mimikit</code>.</p>
-                                </section>
-                                {/*<h2>Documentation</h2>*/}
-                                {/*<section>*/}
-                                {/*    <b>Coming soon</b>: tutorials and documentation for <code>mimikit</code>!*/}
-                                {/*</section>*/}
-                            </div>
-                        </Route>
-                        <Route path={"/music"}>
-                            <div>
-                                <h2>Music</h2>
-                                <p>You'll find more examples on our <a
-                                    href={"https://www.youtube.com/channel/UC_mWCpFGG9xRDbYZMTdwHKw"}>YouTube</a> and <a
-                                    href={"https://soundcloud.com/ktonal"}>SoundCloud</a> channels.</p>
-                                <h2>Highlights</h2>
-                                <div className={"music-grid"}>
-                                    {links.map(link => {
-                                        return (
-                                            <div key={link.url} className={"embed-player"}>
-                                                <ReactPlayer url={link.url}
-                                                    // width={"864px"}
-                                                    // height={"100%"}
-                                                />
-                                                <p>{link.description}</p>
-                                            </div>)
-                                    })}
-                                </div>
-                            </div>
-                        </Route>
-                        <Route path={"/writings"}>
-                            <div>
-                                <h2>Writings</h2>
-                                in progress...
-                            </div>
-                        </Route>
-                        <Route path={"/project-gallery"}>
-                            <div>
-                                <h2>Projects</h2>
-                                in progress...
-                            </div>
-                        </Route>
-                        {/*<Route path={"/members"}>*/}
-                        {/*    <div>*/}
-                        {/*        <h2>Members</h2>*/}
-                        {/*    </div>*/}
-                        {/*</Route>*/}
                     </div>
-                </Switch>
-            </Router>
-            {/*</div>*/}
-            <footer className={"footer"}>
+                    <h1>{"|{"}</h1>
+                    <img src={window.location.origin + "/code-screenshot.png"} className="code-img"
+                         alt="logo"/>
+                    <h1>{"}|"}</h1>
+                </div>
+
+            </div>
+            <footer className={"footer stripe"}>
+                <div className={"motto"}>
+                    <p>K T O N A L </p>
+                    <p>Music with AI from Music to AI in Music for AI as Music of AI for Music to AI</p>
+                </div>
                 <div className={"icon-div"}>
                     <div className={"icons"}>
-                    <a href="https://facebook.com/katonal" target="_blank" rel="noreferrer">
-                        <IconContext.Provider value={{size: 40, color: '#666666'}}>
-                            <FaFacebook/>
-                        </IconContext.Provider>
-                    </a>
-
-                    <a href="https://github.com/ktonal" target="_blank" rel="noreferrer">
-                        <IconContext.Provider value={{size: 40, color: '#666666'}}>
-                            <FaGithub/>
-                        </IconContext.Provider>
-                    </a>
-
-                    <a href="https://youtube.com/" target="_blank" rel="noreferrer">
-                        <IconContext.Provider value={{size: 40, color: '#666666'}}>
-                            <FaYoutube/>
-                        </IconContext.Provider>
-                    </a>
-
-                    <a href="https://soundcloud.com/ktonal" target="_blank" rel="noreferrer">
-                        <IconContext.Provider value={{size: 40, color: '#666666'}}>
-                            <FaSoundcloud/>
-                        </IconContext.Provider>
-                    </a>
-                        </div>
+                        <a href="https://facebook.com/katonal" target="_blank" rel="noreferrer">
+                            <IconContext.Provider value={{size: 40, color: '#CCC'}}>
+                                <FaFacebook/>
+                            </IconContext.Provider>
+                        </a>
+                        <a href="https://youtube.com/" target="_blank" rel="noreferrer">
+                            <IconContext.Provider value={{size: 40, color: '#CCC'}}>
+                                <FaYoutube/>
+                            </IconContext.Provider>
+                        </a>
+                        <a href="https://soundcloud.com/ktonal" target="_blank" rel="noreferrer">
+                            <IconContext.Provider value={{size: 40, color: '#CCC'}}>
+                                <FaSoundcloud/>
+                            </IconContext.Provider>
+                        </a>
+                        <a href="https://github.com/ktonal" target="_blank"
+                           rel="noreferrer">
+                            <IconContext.Provider value={{size: 40, color: '#CCC'}}>
+                                <FaGithub/>
+                            </IconContext.Provider>
+                        </a>
+                    </div>
+                    <p>&#169; {new Date().getFullYear() + " ktonal"}</p>
                 </div>
-                <p>&#169; {new Date().getFullYear() + " ktonal"}</p>
             </footer>
         </div>
     )
