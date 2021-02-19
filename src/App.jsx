0fdc0d45d503a432e31f66b23ab34fb13252fdd1
Antoine Daurat <antoinedaurat@gmail.com>
2021-02-19 11:13:15 +0100

    refactor and make stripes expand/collapse

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import {
-    BrowserRouter as Router, Switch, Route, Link
-} from 'react-router-dom';
 import ReactPlayer from "react-player";
 import {IconContext} from 'react-icons';
 import {FaGithub, FaFacebook, FaSoundcloud, FaYoutube, FaChevronLeft, FaChevronRight} from 'react-icons/fa';
@@ -10,103 +7,174 @@ import './App.scss';
 import {links} from './music-links';
 
 
+function Stripe(props) {
+    const [isOpen, setOpen] = React.useState(false);
+    const isEven = (props.index % 2) === 0;
+    return (
+        <div className={"stripe" + (isEven ? " positive" : " negative")}>
+            <div className={"header"}>
+                {isEven
+                    ? <>{[props.title, props.summary]}</>
+                    : <>{[props.summary, props.title]}</>
+                }
+            </div>
+            <p className={"more"} onClick={() => {
+                setOpen(!isOpen)
+            }}>{isOpen ? "Ok" : "Show Me More"}</p>
+            {isOpen && props.content}
+        </div>
+    )
+}
+
+function Title(props) {
+    return (
+        <div className={"title"} children={props.children}>
+        </div>
+    )
+}
+
+function Summary(props) {
+    return (
+        <div className={"summary"} children={props.children}>
+        </div>
+    )
+}
+
+function Content(props) {
+    return (
+        <div className={"content"} children={props.children}>
+        </div>
+    )
+}
+
+
 function App() {
-    const [faqIndex, setFaqIndex] = React.useState(-1);
     const [linkIdx, setLinkIdx] = React.useState(0);
     return (
         <div className="main">
-            <div className={"ktonal stripe"}>
-                {/*<div className={"top"}><h2 style={{margin: "0 auto", width: "max-content"}}>K T O N A L</h2></div>*/}
-                <div className={"header"}>
-                    <div className={"what-the-k"}>
+            <header className={"top stripe"}>
+                <img src={window.location.origin + "/k-circle-bw.svg"} className="k-logo"
+                     alt="logo"/>
+                <h1>T O N A L</h1>
+            </header>
+            <Stripe
+                index={0}
+                title={
+                    <Title>
                         <h1>What the </h1>
                         <img src={window.location.origin + "/k-circle-bw.svg"} className="k-logo"
-                         alt="logo"/>
+                             alt="logo"/>
                         <h1> ? </h1>
-                    </div>
-                    <div className={"faq description"}>
-                        {/*<div className={"answer"}>*/}
-                        <p>ktonal is a group of composers dedicated to AI-powered sound generation.</p>
+                    </Title>
+                }
+                summary={
+
+                    <Summary>
+                        <p>KTONAL is a group of composers dedicated to AI-powered sound generation.</p>
                         <p>We conduct our own research, write code and compose music.</p>
-                        {/*</div>*/}
-                    </div>
-                </div>
-                {/*<div>*/}
-                {/*    {faqIndex === 0 &&*/}
-                {/*    <div className={"answer"}>*/}
-                {/*        <p>ktonal is a group of composers dedicated to AI-powered sound generation.</p>*/}
-                {/*        <p>We conduct our own research, write code and compose music.</p>*/}
-                {/*    </div>}*/}
-                {/*    {faqIndex === 1 &&*/}
-                {/*    <div className={"answer"}>*/}
-                {/*        <p>We combine a developers perspective with composers interests.</p>*/}
-                {/*        <p>Though AI opens a whole new universe of possibilities, few systems are taylored to*/}
-                {/*            experimental artistic usage.</p>*/}
-                {/*        <p>Current AI/Deep-Learning systems for sounds and music are huge, expensive and hard to*/}
-                {/*            personalize.</p>*/}
-                {/*        <p>That sucks...</p>*/}
-                {/*        <p>We develop accessible, open-source tools for our own work and anybody interested.</p>*/}
-                {/*    </div>}*/}
-                {/*    {faqIndex === 2 &&*/}
-                {/*    <div className={"answer"}>*/}
-                {/*        <p>Spell the "k", then say "tonal".</p>*/}
-                {/*        <p>The "k" stands for the german word for AI : "Künstliche Intelligenz".</p>*/}
-                {/*    </div>}*/}
-                {/*    {faqIndex === 3 &&*/}
-                {/*    <div className={"answer"}>*/}
-                {/*        <p>*/}
-                {/*            Antoine Daurat*/}
-                {/*        </p>*/}
-                {/*        <p>*/}
-                {/*            Björn Erlach*/}
-                {/*        </p>*/}
-                {/*        <p>*/}
-                {/*            Roberto Fausti*/}
-                {/*        </p>*/}
-                {/*        <p>*/}
-                {/*            Genoël von Lilienstern*/}
-                {/*        </p>*/}
-                {/*        <p>*/}
-                {/*            Dohi Moon*/}
-                {/*        </p>*/}
-                {/*    </div>}*/}
-                {/*</div>*/}
-            </div>
-            <div className={"music stripe"}>
-                <div className={"intro description"}>
-                    <p>Lorem Ipsum, music music.</p>
-                    <p>Follow us on {"  "}
-                        <a href="https://youtube.com/" target="_blank" rel="noreferrer">
-                            YouTube
-                        </a>{" "}and{" "}
-                        <a href="https://soundcloud.com/ktonal" target="_blank" rel="noreferrer">
-                            SoundCloud
-                        </a>
-                        {" "}to get the latest tunes.</p>
-                </div>
-                <div className={"player"}>
+                    </Summary>
+                }
+                content={
+                    <Content>
+                        <div className={"q&a"}>
+                            <h3 className={"question"}>What's KTONAL?</h3>
+                            <div className={"answer"}>
+                                <p>ktonal is a group of composers dedicated to AI-powered sound generation.</p>
+                                <p>We conduct our own research, write code and compose music.</p>
+                            </div>
+                        </div>
+                        <div className={"q&a"}>
+                            <h3 className={"question"}>What's our deal?</h3>
+                            <div className={"answer"}>
+                                <p>We combine a developers perspective with composers interests.</p>
+                                <p>Though AI opens a whole new universe of possibilities, few systems are taylored to
+                                    experimental artistic usage.</p>
+                                <p>Current AI/Deep-Learning systems for sounds and music are huge, expensive and hard to
+                                    personalize.</p>
+                                <p>That sucks...</p>
+                                <p>We develop accessible, open-source tools for our own work and anybody interested.</p>
+                            </div>
+                        </div>
+
+                        <div className={"q&a"}>
+                            <h3 className={"question"}>How do you pronounce "ktonal"?</h3>
+                            <div className={"answer"}>
+                                <p>Spell the "k", then say "tonal".</p>
+                                <p>The "k" stands for the german word for AI : "Künstliche Intelligenz".</p>
+                            </div>
+                        </div>
+
+                        <div className={"q&a"}>
+                            <h3 className={"question"}>Who is KTONAL?</h3>
+                            <div className={"answer"}>
+                                <p>
+                                    Antoine Daurat
+                                </p>
+                                <p>
+                                    Björn Erlach
+                                </p>
+                                <p>
+                                    Roberto Fausti
+                                </p>
+                                <p>
+                                    Genoël von Lilienstern
+                                </p>
+                                <p>
+                                    Dohi Moon
+                                </p>
+                            </div>
+                        </div>
+                    </Content>
+                }
+            />
+            <Stripe
+                index={1}
+                title={
+                    <Title>
+                        <h1>Music</h1>
+                    </Title>
+                }
+                summary={
+                    <Summary>
+                        <p>Lorem Ipsum, music music.</p>
+                        <p>Follow us on {"  "}
+                            <a href="https://youtube.com/" target="_blank" rel="noreferrer">
+                                YouTube
+                            </a>{" "}and{" "}
+                            <a href="https://soundcloud.com/ktonal" target="_blank" rel="noreferrer">
+                                SoundCloud
+                            </a>
+                            {" "}to get the latest tunes.</p>
+                    </Summary>
+                }
+                content={
+                    <div className={"player"}>
                         <span className={"previous"}
                               onClick={() => setLinkIdx(linkIdx > 0 ? (linkIdx - 1) % links.length : links.length - 1)}>
                         <IconContext.Provider value={{size: 40, color: '#666666'}}>
                             <FaChevronLeft/>
                         </IconContext.Provider>
                         </span>
-                    <ReactPlayer url={links[linkIdx].url}
-                                 width={"80%"}
-                                 height={"30vh"}
-                    />
-                    <span className={"next"} onClick={() => setLinkIdx((linkIdx + 1) % links.length)}>
+                        <ReactPlayer url={links[linkIdx].url}
+                                     width={"80%"}
+                                     height={"30vh"}
+                        />
+                        <span className={"next"} onClick={() => setLinkIdx((linkIdx + 1) % links.length)}>
                         <IconContext.Provider value={{size: 40, color: '#666666'}}>
                             <FaChevronRight/>
                         </IconContext.Provider>
                         </span>
-                </div>
-                <h1>Music</h1>
-            </div>
-            <div className={"code stripe"}>
-                <div className={"header"}>
-                    <h1>Code</h1>
-                    <div className={"description"}>
+                    </div>}
+            />
+            <Stripe
+                index={2}
+                title={
+                    <Title>
+                        <h1>Code</h1>
+                    </Title>
+                }
+                summary={
+                    <Summary>
                         <p>We are working on several open-source projects to facilitate and democratize audio generation
                             with state-of-the-art algorithms.</p>
                         <p>You'll find all of them on our {" "}
@@ -116,15 +184,17 @@ function App() {
                             </a>
                             .
                         </p>
-
-                    </div>
-                    <h1>{"|{"}</h1>
-                    <img src={window.location.origin + "/code-screenshot.png"} className="code-img"
-                         alt="logo"/>
-                    <h1>{"}|"}</h1>
-                </div>
-
-            </div>
+                    </Summary>
+                }
+                content={
+                    <Content>
+                        <h1>{"|{"}</h1>
+                        <img src={window.location.origin + "/code-screenshot.png"} className="code-img"
+                             alt="logo"/>
+                        <h1>{"}|"}</h1>
+                    </Content>
+                }
+            />
             <footer className={"footer stripe"}>
                 <div className={"motto"}>
                     <p>K T O N A L </p>
