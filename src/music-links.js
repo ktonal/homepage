c2ddb262d6a7bbc73651b0bfc7b5c769ea27d3aa
Antoine Daurat <antoinedaurat@gmail.com>
2021-08-02 07:26:14 +0200

    add links for Mystere & acid from hell

--- src/music-links.js
+++ src/music-links.js
@@ -1,54 +1,67 @@
 const musicLinks = [
-  {
-    url: "https://www.youtube.com/watch?v=LQUuQAwIs9M",
-    title: "AI can't stop laughing",
-    description:
-      "Generated with mimikit.\nNeural network trained with sound recordings of eight different persons laughing.",
-  },
-  {
-    url: "https://www.youtube.com/watch?v=jCpoKjsS_to",
-    title:
-      "AI Adorno: Jargon der Egitnetlichrtagtkit. Reproduction of speaking voice prosody.",
-    description: "Generated with Alcokit 1.0.\n100 Epochs.",
-  },
-  {
-    url: "https://www.youtube.com/watch?v=-XNQPUyI0iM",
-    title: "AI Etude 2",
-    description:
-      "The sound material is generated with a neural network (from the mimikit package) trained on a Lachenmann's orchestral piece",
-  },
-  {
-    url: "https://www.youtube.com/watch?v=DUH5tSm_kfk",
-    title: "AI scream",
-    description:
-      "Generated with mimikit. \nNeural network trained with sound recordings of eight different persons screaming.",
-  },
-  {
-    url: "https://www.youtube.com/watch?v=HuSXU3LxXEo",
-    title: "AI Etude 1 Prokonsori",
-    description:
-      "The sound material is generated with a neural network (from the mimikit package) trained on a Prokofiev Piano sonata and Pansori (Korean  throat singing)\n",
-  },
+    {
+        url: "https://www.youtube.com/watch?v=t2dAsxSN9iU",
+        title: "Le Mystère des Voix Neuronales",
+        description:
+            "“Le Mysteres des Voix neuronales” is a collectively composed audiovisual fixed-media piece by ktonal, which explores the expressive power and diversity of iconic human voices and images of individuals transformed by generative neural networks.",
+    },
 
-  {
-    url: "https://soundcloud.com/ktonal/cathy-berberian-berio-sequenza-iii-4",
-    title: "Cathy Berberian - Berio Sequenza III",
-    description: "",
-  },
-  {
-    url: "https://soundcloud.com/ktonal/cathy-berberian-sequenza-berio-2",
-    title: "Cathy Berberian - Berio Sequenza III",
-    description: "",
-  },
-  {
-    url: "https://soundcloud.com/ktonal/bruckner-symphony-no-8-2-3",
-    title: "Bruckner Symphony No. 8, 2nd Movement",
-    description: "",
-  },
-  {
-    url: "https://soundcloud.com/ktonal/bruckner-symphony-no-8-2-satz",
-    title: "Bruckner Symphony No. 8, 2nd Movement",
-    description: "",
-  },
+    {
+        url: "https://www.youtube.com/watch?v=LQUuQAwIs9M",
+        title: "AI can't stop laughing",
+        description:
+            "Generated with mimikit.\nNeural network trained with sound recordings of eight different persons laughing.",
+    },
+    {
+        url: "https://www.youtube.com/watch?v=jCpoKjsS_to",
+        title:
+            "AI Adorno: Jargon der Egitnetlichrtagtkit. Reproduction of speaking voice prosody.",
+        description: "Generated with Alcokit 1.0.\n100 Epochs.",
+    },
+    {
+        url: "https://www.youtube.com/watch?v=-XNQPUyI0iM",
+        title: "AI Etude 2",
+        description:
+            "The sound material is generated with a neural network (from the mimikit package) trained on a Lachenmann's orchestral piece",
+    },
+    {
+        url: "https://www.youtube.com/watch?v=DUH5tSm_kfk",
+        title: "AI scream",
+        description:
+            "Generated with mimikit. \nNeural network trained with sound recordings of eight different persons screaming.",
+    },
+    {
+        url: "https://www.youtube.com/watch?v=HuSXU3LxXEo",
+        title: "AI Etude 1 Prokonsori",
+        description:
+            "The sound material is generated with a neural network (from the mimikit package) trained on a Prokofiev Piano sonata and Pansori (Korean  throat singing)\n",
+    },
+    {
+        url: "https://www.youtube.com/watch?v=5wkMC42ynh0",
+        title: "Acid from hell",
+        description:
+            "",
+    },
+
+    {
+        url: "https://soundcloud.com/ktonal/cathy-berberian-berio-sequenza-iii-4",
+        title: "Cathy Berberian - Berio Sequenza III",
+        description: "",
+    },
+    {
+        url: "https://soundcloud.com/ktonal/cathy-berberian-sequenza-berio-2",
+        title: "Cathy Berberian - Berio Sequenza III",
+        description: "",
+    },
+    {
+        url: "https://soundcloud.com/ktonal/bruckner-symphony-no-8-2-3",
+        title: "Bruckner Symphony No. 8, 2nd Movement",
+        description: "",
+    },
+    {
+        url: "https://soundcloud.com/ktonal/bruckner-symphony-no-8-2-satz",
+        title: "Bruckner Symphony No. 8, 2nd Movement",
+        description: "",
+    },
 ];
 export default musicLinks;
